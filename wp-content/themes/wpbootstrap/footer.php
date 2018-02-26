7433c4bc6fbd33ab3329fcd7465687c1a8fa455b
syntaxerrorforbearer <syntaxerrorforbearer@gmail.com>
2018-02-26 16:00:02 -0500

    added sidebar widget and archive page

--- wp-content/themes/wpbootstrap/footer.php
+++ wp-content/themes/wpbootstrap/footer.php
@@ -1,3 +1,13 @@
+    <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+      <?php if(is_active_sidebar('sidebar')): ?>
+        <?php dynamic_sidebar('sidebar'); ?>
+      <?php endif; ?>
+    </div><!-- /.blog-sidebar -->
+
+    </div><!-- /.row -->
+
+    </div><!-- /.container -->
+    
     <footer class="blog-footer">
       <p>&copy <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></p>
       <p>
@@ -7,6 +17,6 @@
     <?php wp_footer(); ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
   </body>
 </html>
--- wp-content/themes/wpbootstrap/functions.php
+++ wp-content/themes/wpbootstrap/functions.php
@@ -4,10 +4,35 @@
 require_once get_template_directory() . '/wp-bootstrap-navwalker.php';
 
 
+// Theme Support
  function wpb_theme_setup(){
+   add_theme_support('post-thumbnails');
+
+   // Nav Menus
    register_nav_menus( array(
   	'primary' => __( 'Primary Menu'),
-  ) );
+  ));
  }
 
  add_action('after_setup_theme', 'wpb_theme_setup');
+
+ // Excerpt Length Control
+ function set_excerpt_length(){
+   return 45;
+ }
+
+ add_filter('excerpt_length', 'set_excerpt_length');
+
+ // Widgets Locations
+ function wpb_init_widgets($id){
+   register_sidebar(array(
+     'name' => 'Sidebar',
+     'id'   => 'sidebar',
+     'before_widget'  => '<div class="sidebar-modue">',
+     'after_widget'   => '</div>',
+     'before_title'   => '<h4>',
+     'after_title'    => '</h4>'
+   ));
+ }
+
+ add_action('widgets_init', 'wpb_init_widgets');
