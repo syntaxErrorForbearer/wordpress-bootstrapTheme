a04a9ed124ebd747c00b583532e2ff97f88a3401
syntaxerrorforbearer <syntaxerrorforbearer@gmail.com>
2018-02-27 05:17:47 -0500

    Added post formats - i.e. gallery and asides

--- wp-content/themes/wpbootstrap/functions.php
+++ wp-content/themes/wpbootstrap/functions.php
@@ -1,18 +1,20 @@
 <?php
-
  // Register Custom Navigation Walker
-require_once get_template_directory() . '/wp-bootstrap-navwalker.php';
+ require_once get_template_directory() . '/wp-bootstrap-navwalker.php';
 
-
-// Theme Support
- function wpb_theme_setup(){
+  // Theme Support
+  function wpb_theme_setup(){
    add_theme_support('post-thumbnails');
 
    // Nav Menus
    register_nav_menus( array(
   	'primary' => __( 'Primary Menu'),
-  ));
- }
+    ));
+
+    // Post Formats
+    //add_theme_support('post-formats', array('aside', 'gallery'));
+    add_theme_support( 'post-formats', array( 'aside', 'gallery' ) );
+  }
 
  add_action('after_setup_theme', 'wpb_theme_setup');
 
